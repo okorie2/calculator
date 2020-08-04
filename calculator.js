3c720047cb9c6c7bae07296775a94547afe6ff8e
okorie2 <44427128+okorie2@users.noreply.github.com>
2020-08-04 14:08:10 +0100

    done

--- calculator.js
+++ calculator.js
@@ -2,45 +2,30 @@
   const
   buttons = document.getElementsByTagName("button"),
   screen = document.getElementById('screen');
-  
-
-Array.from(buttons).forEach(button =>
-  button.addEventListener("click", showNumber));
-
-function showNumber(event) {
-  const button = event.target;
-  if(screen.innerHTML==0){
-    screen.innerHTML=button.value;
-  }
-   else if (button.value != 5 ) { screen.innerHTML += button.value; }
-  else  {screen.innerHTML +=5 ;}
-}
-function doMath(){
-          screen.innerHTML=eval(screen.innerHTML)
-        }
-        function clearAll(){
-          screen.innerHTML=;
-        }
+  Array.from(buttons).forEach(button =>
+    button.addEventListener("click", showNumber));
+    function showNumber(event) {
+      const button = event.target;
+      if(screen.innerHTML==0){
+        screen.innerHTML=button.value;
+      }
+      else if (button.value != 5 ) {
+        screen.innerHTML += button.value;
+      }
+      else  {screen.innerHTML +=5
+        ;
+      }
+    }
+    function doMath(){
+      screen.innerHTML=eval(screen.innerHTML)
+    }
+    function clearAll(){
+      screen.innerHTML=0;
+    }
+    function del(){
+      screen.innerHTML= screen.innerHTML.toString().slice(0, -1);
+    }
+    
         
 
-        /*
-        buttons = document.getElementsByTagName("button"),
-        screen = document.getElementById('screen');
-        class calculator {
-          constructor(previousContent, presentContent){
-            this.previousContent= screen;
-            this.presentContent=presentContent;
-          }
-          showNumber(){
-            if (buttons.value!=6){
-              screen.innerHTMl=buttons.value;
-            }
-          }
-        }
-        
-        Array.from(buttons).forEach(button =>
-          button.addEventListener("click",() =>{
-            calculator.showNumber;
-
-          }));*/
-        
+       
